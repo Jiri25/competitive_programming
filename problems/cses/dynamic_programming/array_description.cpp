d82dccdd715c8b2f204f925ef66a4372c44ab093
Jiri25 <ilovemylittlecrab15oct@gmail.com>
2026-06-18 11:38:28 +0630

    Modified Array Description CSES Solution

--- problems/cses/dynamic_programming/array_description.cpp
+++ problems/cses/dynamic_programming/array_description.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
+#include <vector> 
 #include <algorithm> 
-#include <cstring> 
 using namespace std;
 typedef long long ll; 
- 
+
 constexpr int mod = 1e9 + 7; 
-ll dp[2][105]; 
- 
-void init(int num, int m) {
-    if(num == 0) {
-        fill(dp[1] + 1, dp[1] + m + 1, 1); 
+
+void init(vector<ll>& arr, int val) {
+    if(val != 0) {
+        arr[val] = 1; 
     }
-    else dp[1][num] = 1; 
+    else {
+        fill(arr.begin() + 1, arr.end() - 1, 1); 
+    }
 }
- 
+
 int main() {
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false); 
     cin.tie(nullptr); 
- 
+
     int n, m; 
     cin >> n >> m; 
- 
-    int num; 
-    cin >> num; 
-    init(num, m); 
- 
-    bool flag = 0; 
- 
+
+    vector<ll> prev(m + 2); 
+    vector<ll> curr(m + 2); 
+
+    int val; 
+    cin >> val; 
+
+    init(prev, val); 
+    
     while(--n) {
-        memset(dp[flag] + 1, 0, m * sizeof(ll)); 
- 
-        cin >> num; 
-        if(num == 0) {
-            for(int i = 1; i <= m; ++i) {
-                dp[flag][i] = dp[flag ^ 1][i - 1] + dp[flag ^ 1][i] + dp[flag ^ 1][i + 1]; 
-                if(dp[flag][i] >= mod) dp[flag][i] %= mod; 
-            }
+        fill(curr.begin(), curr.end(), 0); 
+        cin >> val; 
+
+        if(val != 0) {
+            curr[val] = (prev[val] + prev[val - 1] + prev[val + 1]) % mod; 
         }
         else {
-            dp[flag][num] = dp[flag ^ 1][num - 1] + dp[flag ^ 1][num] + dp[flag ^ 1][num + 1];
-            if(dp[flag][num] >= mod) dp[flag][num] %= mod; 
+            for(int i = 1; i <= m; ++i) {
+                curr[i] = (prev[i] + prev[i - 1] + prev[i + 1]) % mod; 
+            }
         }
- 
-        flag ^= 1; 
+
+        swap(prev, curr); 
     }
- 
+
     ll sum = 0; 
-    for(int i = 1; i <= m; ++i) {
-        sum += dp[flag ^ 1][i]; 
-        if(sum >= mod) sum %= mod; 
-    }
- 
+    for(int i : prev) sum = (sum + i) % mod; 
+    
     cout << sum << endl; 
     return 0; 
 }
